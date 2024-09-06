bed4c130ca99d90ed961278fe38a2f837e0fe954
missusk <memaria210@gmail.com>
2024-09-06 17:15:37 +0530

    Update AgentChat.js

--- frontend/src/components/AgentChat.js
+++ frontend/src/components/AgentChat.js
@@ -5,7 +5,7 @@ import '../styles/Chat.css';
 import { FaSearch } from 'react-icons/fa'; 
 import io from 'socket.io-client';
 
-const socket = io('${process.env.REACT_APP_API_URL}', {
+const socket = io(`${process.env.REACT_APP_API_URL}`, {
   withCredentials: true,
   transports: ['websocket'],
 });
